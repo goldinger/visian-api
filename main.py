7b3142ac27b1655844fb85d1a3bf5c3ef4e90e72
amine <sghir.ma@gmail.com>
2019-10-23 15:49:53 +0200

    update

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ def project(project_id):
             cursor.execute("select * from Documents where tasktypeid = " + str(int(record.get('tasktypeid', 0))))
             documents = to_dict(cursor.column_names, cursor.fetchall())
             record['documents'] = documents
+            cursor.execute("SELECT Labels.id, Labels.name, Labels.color FROM Labels JOIN LabelTaskType ON Labels.id = LabelTaskType.labelid where LabelTaskType.tasktypeid = " + str(int(record.get('tasktypeid', 0))))
+            labels = to_dict(cursor.column_names, cursor.fetchall())
+            record['labels'] = labels
         response = jsonify(records)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
@@ -64,6 +67,9 @@ def entity(entity_id):
             cursor.execute("select * from Documents where tasktypeid = " + str(int(record.get('tasktypeid', 0))))
             documents = to_dict(cursor.column_names, cursor.fetchall())
             record['documents'] = documents
+            cursor.execute("SELECT Labels.id, Labels.name, Labels.color FROM Labels JOIN LabelTaskType ON Labels.id = LabelTaskType.labelid where LabelTaskType.tasktypeid = " + str(int(record.get('tasktypeid', 0))))
+            labels = to_dict(cursor.column_names, cursor.fetchall())
+            record['labels'] = labels
         response = jsonify(records)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
@@ -204,6 +210,60 @@ def get_projects_by_entity_id(entity_id):
             connection.close()
             cursor.close()
             print("MySQL connection is closed")
+
+
+@app.route('/projects', methods=['PUT'])
+def init_project():
+    if request.method == 'PUT':
+        body = request.get_json(force=True)
+        entity_id = body.get('entityId')
+        title = body.get('title')
+        description = body.get('description')
+        if entity_id is None or title is None:
+            response = jsonify({
+                "status": "KO",
+                "message": "please specify entityId and title"
+            })
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+
+        try:
+            connection = mysql.connector.connect(host='213.32.19.136',
+                                                 database='visian',
+                                                 user='visian',
+                                                 password='visian')
+            cursor = connection.cursor()
+            cursor.execute("select * from TaskTypes")
+            task_types = cursor.fetchall()
+            task_types = to_dict(cursor.column_names, task_types)
+            task_types_ids = [x.get('id') for x in task_types]
+            if description is None:
+                description = ''
+            cursor.execute("insert into Projects (title, entity, description) VALUES ('" + title + "', " + str(entity_id) + ", '" + description + "')")
+            connection.commit()
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            project_id = cursor.fetchone()[0]
+            for task_type_id in task_types_ids:
+                cursor.execute('insert into Tasks (project, tasktype) values (' + str(project_id) + ', ' + str(task_type_id) + ')')
+                connection.commit()
+
+            response = jsonify({
+                "status": "OK"
+            })
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+        except Error as e:
+            print("Error updating data from MySQL table", e)
+            response = jsonify({
+                "status": "KO"
+            })
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            return response
+        finally:
+            if (connection.is_connected()):
+                connection.close()
+                cursor.close()
+                print("MySQL connection is closed")
 
 
 if __name__ == '__main__':
